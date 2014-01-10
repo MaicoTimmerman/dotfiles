1b21d27181ed46f74edf0d2b677b8ff830503e14
Maico Timmerman <maico.timmerman@gmail.com>
2014-01-10 12:05:56 +0100

    Added rename script, see inside for instructions.

---
+++ rename.sh
@@ -0,0 +1,31 @@
+# Rename script by Maico Timmerman
+# Usage:
+# rename.sh $1 $2 check the regex expressions $1 and $2.
+# rename.sh -x $1 $2 rename all files using the given regex.
+case $1 in
+    -x)
+        shift
+        for f in *; do
+            newname=`echo "$f" | sed "s/$1/$2/g"`
+            if [ -f $newname ];
+            then
+                echo "Cannot rename $f, $newname already exists"
+            else
+                mv "$f" "$newname"
+            fi
+        done
+        cmd="ls -h --color=auto"
+        eval $cmd
+        exit 0
+esac
+
+for f in *; do
+    newname=`echo "$f" | sed "s/$1/$2/g"`
+    if [ -f $newname ];
+    then
+        echo "Cannot rename $f, $newname already exists"
+    else
+        echo mv "$f" "$newname"
+    fi
+done
+echo "use '~/rename.sh -x $1 $2' to executed the command"
