0a1ac7ed84531f9082b65909bed7451fe0c52f75
Maico Timmerman <maico.timmerman@gmail.com>
2014-04-29 18:36:27 +0200

    finish setup

--- setup.sh
+++ setup.sh
@@ -8,6 +8,8 @@ ORIGINAL_DIRCOLORS=$HOME/.dircolors
 ORIGINAL_PROFILE=$HOME/.profile
 ORIGINAL_GITCONFIG=$HOME/.gitconfig
 ORIGINAL_VIMRC=$HOME/.vimrc
+ORIGINAL_GNOME_TERMINAL=$HOME/.gconf/apps/gnome-terminal
+ORIGINAL_FONTS=$HOME/.fonts
 PACKAGES=(
     vim git tig tree htop synapse google-chrome-stable gparted skype audacity powertop zip xclip vlc \
     valgrind unrar unzip ipython python3 qalculate openssh-server newsbeuter keepass2 imagemagick \
@@ -24,6 +26,7 @@ $ORIGINAL_DIRCOLORS \
 $ORIGINAL_PROFILE \
 $ORIGINAL_GITCONFIG \
 $ORIGINAL_VIMRC \
+$ORIGINAL_GNOME_TERMINAL \
 $BACKUP_DIR
 
 # Setup Symlinks for all files
@@ -33,6 +36,8 @@ ln -sf $PWD/bash/dircolors $ORIGINAL_DIRCOLORS
 ln -sf $PWD/bash/profile $ORIGINAL_PROFILE
 ln -sf $PWD/git/gitconfig $ORIGINAL_GITCONFIG
 ln -sf $PWD/vim/vimrc $ORIGINAL_VIMRC
+ln -sf $PWD/gnome-terminal $ORIGINAL_GNOME_TERMINAL
+ln -sf $PWD/fonts $ORIGINAL_FONTS
 
 # Install the newest updates for already installed packages
 sudo apt-get --yes --force-yes upgrade
@@ -41,27 +46,38 @@ sudo dpkg --add-architecture i386
 
 # Google Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo sh -c 'echo "deb http://dl.google.com/linux/talkplugin/deb/ stable main" >> /etc/apt/sources.list.d/google-talkplugin.list'
+if [[ ! -e "/etc/apt/sources.list.d/google-chrome.list" ]];
+then
+    sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+fi
+
+if [[ ! -e "/etc/apt/sources.list.d/google-talkplugin.list" ]];
+then
+    sudo sh -c 'echo "deb http://dl.google.com/linux/talkplugin/deb/ stable main" >> /etc/apt/sources.list.d/google-talkplugin.list'
+fi
 
 # Skype
 sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+
+# Java
+sudo add-apt-repository ppa:webupd8team/java
 
 # Pipeline
 sudo apt-add-repository ppa:pipelight/stable
 
 # Dropbox
 sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
-sudo sh -c 'echo "deb http://linux.dropbox.com/ubuntu/ trusty main" >> /etc/apt/sources.list.d/dropbox.list'
-
-# Java
-sudo add-apt-repository ppa:webupd8team/java
+if [[ ! -e "/etc/apt/sources.list.d/dropbox.list" ]];
+then
+    sudo sh -c 'echo "deb http://linux.dropbox.com/ubuntu/ trusty main" >> /etc/apt/sources.list.d/dropbox.list'
+fi
 
 sudo apt-get update
-sudo apt-get --yes --force-yes install ${PACKAGES[@]}
 
-wget -q -O - http://ftp.musicbrainz.org/pub/musicbrainz/picard/picard-1.2.tar.gz | tar zxvf -
-sudo python picard-1.2/setup.py install
+for item in ${PACKAGES[*]}
+do
+    sudo apt-get --yes --force-yes install $ITEM
+done
 
 sudo pipelight-plugin --enable silverlight
 
