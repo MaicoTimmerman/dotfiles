6fe0334097835c84e8227ec2d35d021db8aa6f5c
Maico Timmerman <maico.timmerman@gmail.com>
2014-10-03 16:38:31 +0200

    Added fish config

---
+++ fish/aliases.fish
@@ -0,0 +1,23 @@
+#Maico timmerman alias
+#User specific aliases and functions
+
+# alias to edit all aliases and automatically load them in
+# after editting.
+alias editalias="vim /home/maico/dotfiles/fish/aliases.fish; source /home/maico/dotfiles/fish/aliases.fish"
+alias editi3="vim /home/maico/.i3/config; i3-msg reload"
+alias editfish="vim /home/maico/dotfiles/fish/config.fish; source /home/maico/dotfiles/fish/config.fish"
+
+#cd aliases
+alias cduva="cd /media/maico/uva/"
+alias cdics="cd /home/maico/uva/ics"
+alias cdac="cd /home/maico/uva/ac"
+
+# More aliases!
+alias ls="ls -h --color=auto"
+alias clearl="clear;ls"
+alias xclip="xclip -selection clipboard"
+alias poweroff="sudo poweroff"
+alias die="sudo poweroff"
+alias reboot="sudo reboot"
+alias html="google-chrome"
+alias uvavpn='sudo /usr/local/nc/ncsvc -h uvavpn.uva.nl -u 10542590 -r users -f /usr/local/nc/certificaat-uvavpn.der'
---
+++ fish/config.fish
@@ -0,0 +1,22 @@
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
+
+# Theme
+# set fish_theme agnoster
+set fish_theme ocean
+# set fish_theme bobthefish
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Example format: set fish_plugins autojump bundler
+set fish_plugins git
+
+# Path to your custom folder (default path is $FISH/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
+
+set -g theme display_user yes
+set -g default_user maico
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
+. $HOME/dotfiles/fish/aliases.fish
