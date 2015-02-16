0ddaa72b8123efbd4f21fe6be35a3f376acdc3e8
Maico Timmerman <maico.timmerman@gmail.com>
2015-02-16 14:21:32 +0100

    Work in progress

--- setup_server.sh
+++ setup_server.sh
@@ -10,9 +10,6 @@ ORIGINAL_GITCONFIG=$HOME/.gitconfig
 ORIGINAL_VIMRC=$HOME/.vimrc
 ORIGINAL_GNOME_TERMINAL=$HOME/.gconf/apps/gnome-terminal
 ORIGINAL_FONTS=$HOME/.fonts
-PACKAGES=(
-    vim git tig git-flow tree htop powertop zip unrar unzip python3 ctags python3-numpy 
-)
 
 #Move all original rc files to a backup dir.
 mkdir -p $BACKUP_DIR
@@ -28,24 +25,14 @@ $ORIGINAL_FONTS \
 $BACKUP_DIR
 
 # Setup Symlinks for all files
-# ln -sf $PWD/bash/bashrc $ORIGINAL_BASHRC
-# ln -sf $PWD/bash/bash_aliases $ORIGINAL_BASHALIASES
-# ln -sf $PWD/bash/dircolors $ORIGINAL_DIRCOLORS
-# ln -sf $PWD/bash/profile $ORIGINAL_PROFILE
-# ln -sf $PWD/git/gitconfig $ORIGINAL_GITCONFIG
-# ln -sf $PWD/vim/vimrc $ORIGINAL_VIMRC
-# ln -sf $PWD/gnome-terminal $ORIGINAL_GNOME_TERMINAL
-# ln -sf $PWD/fonts $ORIGINAL_FONTS
-
-sudo apt-get update
-
-# Install the newest updates for already installed packages
-sudo apt-get --yes --force-yes upgrade
-
-for i in "${PACKAGES[@]}"
-do
-    sudo apt-get --yes --force-yes install $i
-done
+ln -sf $PWD/bash/bashrc $ORIGINAL_BASHRC
+ln -sf $PWD/bash/bash_aliases $ORIGINAL_BASHALIASES
+ln -sf $PWD/bash/dircolors $ORIGINAL_DIRCOLORS
+ln -sf $PWD/bash/profile $ORIGINAL_PROFILE
+ln -sf $PWD/git/gitconfig $ORIGINAL_GITCONFIG
+ln -sf $PWD/vim/vimrc $ORIGINAL_VIMRC
+ln -sf $PWD/gnome-terminal $ORIGINAL_GNOME_TERMINAL
+ln -sf $PWD/fonts $ORIGINAL_FONTS
 
 # Setup Vundle for vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
