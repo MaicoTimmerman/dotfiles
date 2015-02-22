b986dc91d1bd6edb09d27c2df390c6896512c368
Maico Timmerman <maico.timmerman@gmail.com>
2015-02-22 18:39:36 +0100

    Bind functions dont work in vi-mode

--- fish/config.fish
+++ fish/config.fish
@@ -25,7 +25,7 @@ set VIRTUAL_ENV_DISABLE_PROMPT yes
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins git vundle git-flow gi bundler jump vi-mode
+set fish_plugins git vundle git-flow gi bundler jump
 
 # Used for defining PATH additions
 # set -U fish_user_paths $fish_user_paths /usr/bin/...
